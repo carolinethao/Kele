5a9b80977364a72e8f9e420833056b3aeff7494b
Caroline Nguyen <caroline.nguyen@outlook.com>
2018-09-19 22:29:59 -0500

    c07 - completed

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -13,4 +13,9 @@ module Roadmap
   def get_checkpoint(checkpoint_id)
     response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" =>  @auth_token })
   end
+
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
 end
